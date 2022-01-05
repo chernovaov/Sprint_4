e16e537d33b07cd813247efc102c430077674a02
Olesya Chernova <93104132+chernovaov@users.noreply.github.com>
2022-01-05 15:09:05 +0300

    Revert "first commit"
    This reverts commit c97dd96a63f6bcf7a7d87ce785f561665c4ce9c5.

--- src/main/java/praktikum/Praktikum.java
+++
@@ -1,9 +0,0 @@
-package praktikum;
-
-public class Praktikum {
-    public static void main(String[] args) {
-        String name = "Николаев Иван";
-        Account account = new Account(name);
-        account.checkNameToEmboss();
-    }
-}
